ae407a66faa611d07dec1ee5b81aa78f1693b048
Luiz Felipe Borges Santos Carvalho <103582640+LuizFelipeBSC@users.noreply.github.com>
2022-10-31 18:51:18 -0300

    V1

---
+++ AumentaEm1oValor.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner ler = new Scanner(System.in);
+        int A = ler.nextInt();
+
+        System.out.print(++A);
+
+
+    }
+}
